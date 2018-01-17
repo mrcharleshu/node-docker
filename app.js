ebd5f786b5a08c6123e39dc178bd5f71d7fe50e0
Charles <mrcharleshu@gmail.com>
2018-01-17 17:28:36 +0800

    add other API

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/api/v1/test/available', function (req, res) {
+  res.send('This is another API : available');
+});
+
+app.get('/api/v1/test/health', function (req, res) {
+  res.send('Application runs well!');
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
